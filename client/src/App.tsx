3590ba1969c8efbd525bbe794fa56a1c8bdc5d77
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-02-08 17:52:10 -0500

    Adding sockets to the app **are working**

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,80 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import io from "socket.io-client";
+
+interface Message {
+  room: string;
+  message: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [socket, setSocket] = useState<any | null>(null);
+  const [room, setRoom] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:81");
+    newSocket.on("connect", () => {
+      setSocket(newSocket);
+    });
+    return () => {
+      newSocket.disconnect();
+    };
+  }, []);
+
+  const joinRoom = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    socket.emit("joinRoom", room);
+  };
+
+  const leaveRoom = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    socket.emit("leaveRoom", room);
+  };
+
+  const sendMessage = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    socket.emit("messageRoom", { room, message });
+    setMessage("");
+  };
+
+  useEffect(() => {
+    if (!socket) {
+      return;
+    }
+    socket.on("message", (payload: Message) => {
+      setMessages((prev) => [...prev, payload]);
+    });
+  }, [socket]);
 
   return (
     <div className="App">
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <form onSubmit={joinRoom}>
+          <input
+            type="text"
+            value={room}
+            onChange={(e) => setRoom(e.target.value)}
+          />
+          <button type="submit">Join Room</button>
+        </form>
+        <form onSubmit={leaveRoom}>
+          <button type="submit">Leave Room</button>
+        </form>
+        <form onSubmit={sendMessage}>
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button type="submit">Send Message</button>
+        </form>
+        <ul>
+          {messages.map((message, index) => (
+            <li key={index}>{message.message}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
