4e551c6755e46ad75a26644bc5ee63b3e0d5bae8
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-02-09 17:40:02 -0500

    Creating rooms list logic, adding custom hook and adding styles

--- client/src/containers/Rooms.tsx
+++ client/src/containers/Rooms.tsx
@@ -1,5 +1,91 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import useSearch from "../hooks/useSearch";
+import { Rooms as RoomsInterface } from "../features/rooms/room.interface";
+import style from "../styles/Rooms.module.css";
+
+const rooms: RoomsInterface[] = [
+  {
+    _id: "63e025961e692de698b862c1",
+    image: "",
+    roomname: "Room 1",
+    description: "Description room 1",
+    creator: {
+      _id: "63e025841e692de698b862bb",
+      name: "Pulga",
+    },
+  },
+  {
+    _id: "63e025961e692de698b862n1",
+    image: "",
+    roomname: "Room 2",
+    description: "Description room 2",
+    creator: {
+      _id: "63e025841e692de698b862ba",
+      name: "Monin",
+    },
+  },
+  {
+    _id: "63e025961e692de698b862a1",
+    image: "",
+    roomname: "Games",
+    description: "Description room 2",
+    creator: {
+      _id: "63e025841e692de698b862ba",
+      name: "Monin",
+    },
+  },
+  {
+    _id: "63e025961e692de698b862q1",
+    image: "",
+    roomname: "Animals",
+    description: "Description room 2",
+    creator: {
+      _id: "63e025841e692de698b862ba",
+      name: "Monin",
+    },
+  },
+];
+
 const Rooms = () => {
-  return <div>room</div>;
+  const { searchTerm, filteredItems, setSearchTerm } = useSearch(rooms);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className={style.rooms_list_cont}>
+      <form onSubmit={(e) => handleSubmit(e)} className={style.form_search}>
+        <input
+          type="text"
+          placeholder="Search room"
+          value={searchTerm}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setSearchTerm(e.target.value)
+          }
+        />
+      </form>
+      <div className={style.rooms_list}>
+        {filteredItems.map((item) => {
+          return (
+            <NavLink
+              to={`/chat/${item._id}`}
+              key={item._id}
+              className={style.rooms_list_item}
+            >
+              <img
+                src={item.image}
+                alt={`${item.roomname} image`}
+                className={style.item_image}
+              />
+              <p className={style.item_name}>{item.roomname}</p>
+            </NavLink>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Rooms;
