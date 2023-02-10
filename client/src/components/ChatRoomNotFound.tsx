c59c789b388d0c6e7f9484f41a4b39a1d6553182
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-02-10 11:09:10 -0500

    Modifying not found rooms styles to styled modules

--- client/src/components/ChatRoomNotFound.tsx
+++ client/src/components/ChatRoomNotFound.tsx
@@ -1,19 +1,11 @@
 import { Outlet } from "react-router-dom";
 import NotFound from "./NotFound";
 import Rooms from "../containers/Rooms";
+import style from "../styles/ChatRoomNotFound.module.css";
 
 const ChatRoomNotFound = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-        height: "100%",
-        width: "100%",
-      }}
-    >
+    <div className={style.chat_not_found}>
       <Rooms />
       <NotFound text="Search a room to start to chat" />
       <Outlet />
