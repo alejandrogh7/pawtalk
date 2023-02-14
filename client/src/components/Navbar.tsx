782882a41d03ac526c31de7d25a7d81e4f5a23a9
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-02-14 13:34:00 -0500

    Creating navbar for sign and adding logic

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+import Chat from "../assets/Chat.svg";
+import User from "../assets/User.svg";
+import AddUser from "../assets/AddUser.svg";
+import style from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={style.nav_cont}>
+      <NavLink to="/chat" className={style.nav_item_cont}>
+        <img src={Chat} alt="chat-image" className={style.nav_item} />
+      </NavLink>
+      <NavLink to="/signin" className={style.nav_item_cont}>
+        <img src={User} alt="user-image" className={style.nav_item} />
+      </NavLink>
+      <NavLink to="/signup" className={style.nav_item_cont}>
+        <img src={AddUser} alt="add-user-image" className={style.nav_item} />
+      </NavLink>
+    </div>
+  );
+};
+
+export default Navbar;
