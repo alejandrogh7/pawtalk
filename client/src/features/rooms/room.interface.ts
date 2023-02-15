991531343d6f9f841d8871499a9ab9138724392f
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-02-15 11:54:40 -0500

    Create interfaces for rooms post and room

--- client/src/features/rooms/room.interface.ts
+++ client/src/features/rooms/room.interface.ts
@@ -1,12 +1,40 @@
+interface User {
+  _id: string;
+  name: string;
+}
+
+interface Creator {
+  _id: string;
+  name: string;
+}
+
+interface Sender {
+  _id: string;
+  name: string;
+}
+
+export interface Post {
+  _id: string;
+  text: string;
+  sender: Sender;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
 export interface Rooms {
   _id: string;
-  image: string;
   roomname: string;
   description: string;
   creator: Creator;
 }
 
-export interface Creator {
+export interface Room {
   _id: string;
-  name: string;
+  roomname: string;
+  description: string;
+  users: User[];
+  creator: Creator;
+  posts: Post[];
+  createdAt: Date;
+  updatedAt: Date;
 }
