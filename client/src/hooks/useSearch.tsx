f04b1036e28aad8ba186e87165e3a752a5630af2
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-02-15 11:53:04 -0500

    Usesearch if I do not recieve items

--- client/src/hooks/useSearch.tsx
+++ client/src/hooks/useSearch.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { Rooms } from "../features/rooms/room.interface";
 
-const useSearch = (items: Rooms[]) => {
+const useSearch = (items: Rooms[] | null) => {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [filteredItems, setFilteredItems] = useState<Rooms[]>([]);
 
   useEffect(() => {
+    if (!items) return;
+
     setFilteredItems(
       items.filter((item) =>
         item.roomname
