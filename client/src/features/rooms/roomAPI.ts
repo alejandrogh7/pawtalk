7a4354676f993d2ca3455a44262fa28baa4f72ff
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-02-17 09:46:17 -0500

    Logic to get rooms

--- client/src/features/rooms/roomAPI.ts
+++ client/src/features/rooms/roomAPI.ts
@@ -2,24 +2,13 @@ import axios from "axios";
 import { Room, Rooms } from "./room.interface";
 
 export const fetchRooms = async (): Promise<Rooms[]> => {
-  const response = await axios.get(`${import.meta.env.VITE_API_URL}/rooms`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const response = await axios.get(`${import.meta.env.VITE_API_URL}/rooms`);
   return response.data;
 };
 
 export const fetchRoom = async (roomID: string): Promise<Room> => {
   const response = await axios.get(
-    `${import.meta.env.VITE_API_URL}/rooms/${roomID}`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
+    `${import.meta.env.VITE_API_URL}/rooms/${roomID}`
   );
   return response.data;
 };
