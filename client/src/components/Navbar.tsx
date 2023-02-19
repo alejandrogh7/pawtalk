2c2b1069563e1798fc856fa35c0374afb9f20455
alejandrogh7 <alejo1garciasosa@gmail.com>
2023-02-19 15:23:02 -0500

    Removing unnecessary data from navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -7,9 +7,9 @@ import style from "../styles/Navbar.module.css";
 const Navbar = () => {
   return (
     <div className={style.nav_cont}>
-      <NavLink to="/chat" className={style.nav_item_cont}>
+      {/* <NavLink to="/chat" className={style.nav_item_cont}>
         <img src={Chat} alt="chat-image" className={style.nav_item} />
-      </NavLink>
+      </NavLink> */}
       <NavLink to="/signin" className={style.nav_item_cont}>
         <img src={User} alt="user-image" className={style.nav_item} />
       </NavLink>
